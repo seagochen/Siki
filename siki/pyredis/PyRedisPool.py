5f1390109208643a268594071642454b9d5f6bf4
Orlando Chen <seagochen@hotmail.com>
2020-02-21 17:32:28 +0800

    redis mem token ready

--- siki/pyredis/PyRedisPool.py
+++ siki/pyredis/PyRedisPool.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Author: Orlando Chen
-# Create: Sep 20, 2018
-# Modifi: Sep 20, 2018
+# Created: Sep 20, 2018
+# LastChg: Feb 20, 2020
 
 import redis
 
@@ -12,6 +12,8 @@ from siki.dstruct.Queue import Queue
 
 
 class PyRedisPool(object):
+
+
 
     def __init__(self, **params):
         """
@@ -31,29 +33,171 @@ class PyRedisPool(object):
         logger.message(p.INFO, msg="redis pool ready")
     
 
+
+
     def set_variable(self, key, val, secs=None, millisecs=None):
+        """
+        stores data to redis
+
+        Args:
+        * [key] str key of data
+        * [val] string value or anything can be cast to bytes
+        * [secs] expiration time in seconds, default is None
+        * [millisecs] expiration time in milliseconds, default is None
+        """
         try:
             redis.Redis(connection_pool=self.pool).set(key, val, secs, millisecs)
         except Exception as e:
             logger.message(p.ERROR, msg="set variable failed", exception=e)
     
 
+
+
+    def set_mulvars(self, dicts, secs=None, millisecs=None):
+        """
+        stores multiply data to redis
+
+        Args:
+        * [dicts(dict)] dictionary type of data with key and value, the key and value should
+            both be string or anything that can be cast to bytes
+        * [secs] expiration time in seconds, default is None
+        * [millisecs] expiration time in milliseconds, default is None
+        """
+        try:
+            r = redis.Redis(connection_pool=self.pool)
+
+            # pipeline
+            pipe = r.pipeline()
+
+            for key, val in dicts.items():
+                pipe.set(key, val, secs, millisecs)
+
+        except Exception as e:
+            logger.message(p.ERROR, msg="set variable(s) failed", exception=e)
+
+
+
+
     def get_variable(self, key):
+        """
+        get variable from redis
+
+        Args:
+        * [key] the key name to the data
+        
+        Returns:
+        * [str/bytes] the data can be string or bytes
+        """
         try:
             return redis.Redis(connection_pool=self.pool).get(key)
         except Exception as e:
             logger.message(p.ERROR, msg="get variable failed", exception=e)
 
+
+
+
+    def get_mulvars(self, keys):
+        """
+        get variables from redis
+        
+        Args:
+        * [key] the key name to the data
+        
+        Returns:
+        * [list] list of the data can be string or bytes
+        """
+
+        try:
+            return redis.Redis(connection_pool=self.pool).mget(*keys)
+        except Exception as e:
+            logger.message(p.ERROR, msg="get variable(s) failed", exception=e)
+
+
     
+
     def ping(self):
+        """
+        ping the server
+        """
         try:
             return redis.Redis(connection_pool=self.pool).ping()
         except Exception as e:
             logger.message(p.ERROR, msg="ping redis failed", exception=e)
 
     
+
+
     def get_keys(self, pattern="*"):
+        """
+        get a list of keys
+
+        Args:
+        * [pattern] pattern to match the keys, default is *
+
+        Returns:
+        * [list] list of keys
+        """
         try:
             return redis.Redis(connection_pool=self.pool).keys(pattern=pattern)
         except Exception as e:
             logger.message(p.ERROR, msg="get keys failed", exception=e)
+
+
+
+
+    def empty(self):
+        """
+        clear all keys
+        """
+        try:
+            redis.Redis(connection_pool=self.pool).flushall()
+        except Exception as e:
+            logger.message(p.ERROR, msg="flush all keys failed", exception=e)
+
+
+
+    
+    def remove(self, keys):
+        """
+        remove a key or list of keys from redis
+
+        Args:
+        * [key(str/list)] a key string or list of key strings to remove
+        """
+        try:
+            if type(keys) is str:
+                redis.Redis(connection_pool=self.pool).delete(keys)
+            elif type(key) is str:
+                redis.Redis(connection_pool=self.pool).delete(*keys)
+            else:
+                logger.message(p.Error, msg="remove key with an invalid key type")
+        except Exception as e:
+            logger.message(p.ERROR, msg="remove key(s) failed", exception=e)
+
+
+
+
+    def exists(self, key):
+        """
+        chekc a key if exists
+        """
+        try:
+            return redis.Redis(connection_pool=self.pool).exists(key)
+        except Exception as e:
+            logger.message(p.ERROR, msg="failed with some uncertain errors", exception=e)
+
+
+
+
+    def set_key_exp(self, key, exp):
+        """
+        set expiration for key
+
+        Args:
+        * [key] string key name
+        * [exp] expiration time in seconds
+        """
+        try:
+            redis.Redis(connection_pool=self.pool).expire(key, exp)
+        except Exception as e:
+            logger.message(p.ERROR, msg="set expiration failed", exception=e)
---
+++ siki/pyredis/RedisDataToken.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# Author: Orlando Chen
+# Created: Feb 21, 2020
+# LastChg: Feb 21, 2020
+
+from siki.basics import TimeTicker as ticker
+from siki.basics import Convert as convert
+from siki.basics import JsonUtils as jutil
+from siki.basics import Exceptions as excepts
+
+from siki.pyredis.PyRedisPool import PyRedisPool
+
+import sys
+import base64
+
+class RedisDataToken(object):
+
+    """
+    {'data':base64/str, 'type':str, 'timestamp':float}
+    """
+
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.data = None
+        self.dtype = None
+        self.timestamp = None
+        self.token = None
+
+
+    def decode(self, token: bytes):
+        """
+        since obtained token from redis, use this method to 
+        convert the bytes into correct data type
+
+        Args:
+        * [token(bytes)] obtained bytes from redis
+
+        Returns:
+        * [token(dict)] contains data, timestamp, type infomation
+        """
+        # reset all
+        self.reset()
+
+        if token is not None:
+            # 1, convert the bytes into dictionary
+            self.token = convert.binary_to_dict(token)
+
+            # 2. assign to attributes
+            self.dtype = self.token["type"]
+            self.timestamp = self.token["timestamp"]
+
+            # 3. if bytes, process them carefully
+            if self.dtype == "bytes":
+                self.data = base64.b64decode(convert.string_to_binary(self.token["data"]))
+            else: # normal data
+                self.data = self.token["data"]
+
+            # 4. return to caller
+            return self.token
+
+        else:
+            raise excepts.NullPointerException("token cannot be a null type")
+
+    
+    def encode(self, data):
+        """
+        convert given data into redis token
+
+        Args:
+        * [data(any)] data can be anything, bytes, int, float or whatever
+
+        Returns:
+        * [token(bytes)] composed token consisted of data, type, timestamp
+        """
+        # reset
+        self.reset()
+
+        if data is None:
+            raise excepts.NullPointerException("data cannot be a null type")
+
+        # 1. create timestamp
+        self.timestamp = ticker.time_since1970()
+
+        # 2. assign bytes to self.data
+        if type(data) is bytes:
+            self.dtype = "bytes"
+            self.data = convert.binary_to_string(base64.b64encode(data))
+        else: # other types
+            self.data = data
+
+            if type(data) is int:
+                self.dtype = "int"
+            elif type(data) is float:
+                self.dtype = "float"
+            elif type(data) is str:
+                self.dtype = "str"
+            elif type(data) is list:
+                self.dtype = "list"
+            elif type(data) is dict:
+                self.dtype = "dict"
+            else:
+                raise excepts.InvalidParamException("{} cannot be processed into string nor bytes"
+                    .format(type(data)))
+
+        # 3. composing a token dictionary type
+        self.token = {"data":self.data, "type":self.dtype, "timestamp":self.timestamp}
+
+        # return to caller
+        return convert.dict_to_binary(self.token)
