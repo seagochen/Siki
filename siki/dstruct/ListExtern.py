fa9b9c86e1a489c094da86affe1c041bb656a68a
Orlando Chen <seagochen@hotmail.com>
2020-02-27 20:56:37 +0800

    split a list is now available

--- siki/dstruct/ListExtern.py
+++ siki/dstruct/ListExtern.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Author: Orlando Chen
 # Created: May 10, 2018
-# LastChg: Feb 19, 2020
+# LastChg: Feb 27, 2020
 
 
-def intersection(setA, setB):
+def intersection(setA: list, setB: list):
     """
     intersection of two lists with basic datatype
     """
@@ -15,7 +15,7 @@ def intersection(setA, setB):
     return setC
 
 
-def union(setA, setB):
+def union(setA: list, setB: list):
     """
     union of two lists with basic datatype
     """
@@ -28,7 +28,7 @@ def union(setA, setB):
     return setC
 
 
-def remove_sets(bigset, subset):
+def remove_sets(bigset: list, subset: list):
     """
     remove items of subset B from big set
     """
@@ -40,7 +40,49 @@ def remove_sets(bigset, subset):
     return setC
 
 
-def clear(sets):
+
+
+def split(sets: list, quantity: int):
+    """
+    splits a list into smaller lists, each of sub list contains 
+    the (quantity) of items
+
+    Args:
+    * [sets] list to split into smaller lists
+    * [quantity] the items in each sub list
+
+    Returns:
+    * [list] a new list of sub lists
+    """
+
+    output = []
+
+    sublist = None
+    max_contains = quantity
+
+    for item in sets:
+
+        if sublist is None:
+            sublist = []
+        
+        if max_contains > 0:
+            sublist.append(item)
+            max_contains = max_contains - 1
+        
+        if max_contains == 0:
+            output.append(sublist)
+            sublist = []
+            max_contains = quantity
+
+    # something left
+    if len(sublist) > 0:
+        output.append(sublist)
+
+    # finished
+    return output
+
+
+def clear(sets: list):
     """
     clear all element from a list
     """
