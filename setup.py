4b3ed459110e92feb4f4111bf5f897534412220b
Orlando Chen <seagochen@hotmail.com>
2020-04-07 10:53:09 +0800

    license changed, added requires

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="siki",
-    version="0.1.4.0",
+    version="0.1.4.2",
     author="Orlando Chen",
     author_email="seagochen@hotmail.com",
     description="A collection of tools that may be used to help users coding with Python in an easy way",
@@ -16,9 +16,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/seagochen/Siki",
     packages=setuptools.find_packages(),
+    install_requires=["pymysql", "redis"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
